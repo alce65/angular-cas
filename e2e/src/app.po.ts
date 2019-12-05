67292f9cae33e4a400d6d3e154b1f8c766a8256d
alce65 <alce65@hotmail.es>
2019-12-05 19:37:29 +0100

    Test e2e

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText() {
-    return element(by.css('cas-root .content span')).getText() as Promise<string>;
+    return element(by.css('cas-header h1')).getText() as Promise<string>;
   }
 }
